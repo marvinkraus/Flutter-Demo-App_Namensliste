a4dbc1c68db13b528efb56db4da5a9f3788f6c41
marvinkraus <marvin.kraus2310@outlook.de>
2022-06-09 10:11:53 +0200

    --- Auto-Git Commit ---

--- lib/pages/addNewPerson.dart
+++ lib/pages/addNewPerson.dart
@@ -7,22 +7,38 @@ class addNewPersonPage extends StatelessWidget {
   addNewPersonPage({super.key});
 
   late String name;
+  late int alter;
+  late String picture;
   //late bedeutet dass zunächst kein Inhalt in Name gespeichert wird und das erst zur Laufzeit passiert
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(5),
+        padding:
+            const EdgeInsets.only(right: 10, left: 10, bottom: 10, top: 10),
         child: Column(children: [
           TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter your name',
+            ),
             onChanged: ((value) {
               name = value;
+            }),
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter your age',
+            ),
+            onChanged: ((value) {
+              alter = int.parse(value);
             }),
           ),
           ElevatedButton(
               onPressed: (() {
                 if (name.isNotEmpty) {
-                  Person p = Person(name: name, alter: 11, picture: "");
+                  Person p = Person(name: name, alter: alter, picture: "");
                   Navigator.of(context).pop(p);
                 }
               }),
